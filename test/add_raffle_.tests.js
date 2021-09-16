a5fc7f99288c289f7cbace4a53d9ccc80475296f
Alexandru <gazdac.alex21@gmail.com>
2021-09-16 22:43:09 +0300

    uncommenting the tests

--- test/add_raffle_.tests.js
+++ test/add_raffle_.tests.js
@@ -1,48 +1,48 @@
-// const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-// describe("Add raffle tests", function () {
+describe("Add raffle tests", function () {
 
-//     let deployerAccount;
-//     let raffleWorld;
-//     let testToken;
+    let deployerAccount;
+    let raffleWorld;
+    let testToken;
 
-//     before(async function () {
-//         const [deployer] = await ethers.getSigners();
-//         deployerAccount = deployer;
-//     });
+    before(async function () {
+        const [deployer] = await ethers.getSigners();
+        deployerAccount = deployer;
+    });
 
-//     beforeEach(async function () {
-//         const RaffleWorld = await ethers.getContractFactory("RaffleWorld");
-//         raffleWorld = await RaffleWorld.deploy();
+    beforeEach(async function () {
+        const RaffleWorld = await ethers.getContractFactory("RaffleWorld");
+        raffleWorld = await RaffleWorld.deploy();
 
-//         const TestToken = await ethers.getContractFactory("TestToken");
-//         testToken = await TestToken.deploy("Test Token", "TKN1");
+        const TestToken = await ethers.getContractFactory("TestToken");
+        testToken = await TestToken.deploy("Test Token", "TKN1");
 
-//     });
+    });
 
-//     it("Should not add a raffle to the raffles array because raffle start day is beyond block timestamp", async function () {
-//         await expect(raffleWorld.setRaffle("Test raffle", "1596663323", testToken.address, "1000000000000", "10", "1000000000", "2"))
-//             .to.be.revertedWith("RaffleWorld: raffle's start date should be in the future!");
-//     });
+    it("Should not add a raffle to the raffles array because raffle start day is beyond block timestamp", async function () {
+        await expect(raffleWorld.setRaffle("Test raffle", "1596663323", testToken.address, "1000000000000", "10", "1000000000", "2"))
+            .to.be.revertedWith("RaffleWorld: raffle's start date should be in the future!");
+    });
 
-//     it("Should not add a raffle to the raffles array because the prize amount is 0", async function () {
-//         await expect(raffleWorld.setRaffle("Test raffle", "1663346190", testToken.address, "0", "10", "1000000000", "2"))
-//             .to.be.revertedWith("RaffleWorld: raffle's prize amount should be greater than 0!");
-//     });
+    it("Should not add a raffle to the raffles array because the prize amount is 0", async function () {
+        await expect(raffleWorld.setRaffle("Test raffle", "1663346190", testToken.address, "0", "10", "1000000000", "2"))
+            .to.be.revertedWith("RaffleWorld: raffle's prize amount should be greater than 0!");
+    });
 
-//     it("Should not add a raffle to the raffles array because the tickets limit is 0", async function () {
-//         await expect(raffleWorld.setRaffle("Test raffle", "1663346190", testToken.address, "1000000000000", "0", "1000000000", "2"))
-//             .to.be.revertedWith("RaffleWorld: raffle's tickets limit should be greater than 0!");
-//     });
+    it("Should not add a raffle to the raffles array because the tickets limit is 0", async function () {
+        await expect(raffleWorld.setRaffle("Test raffle", "1663346190", testToken.address, "1000000000000", "0", "1000000000", "2"))
+            .to.be.revertedWith("RaffleWorld: raffle's tickets limit should be greater than 0!");
+    });
 
-//     it("Should not add a raffle to the raffles array because the contract doesn't have allowance for the prize amount", async function () {
-//         await expect(raffleWorld.setRaffle("Test raffle", "1663346190", testToken.address, "1000000000000", "10", "1000000000", "2"))
-//             .to.be.revertedWith("RaffleWorld: Insufficient tokens for prize poll");
-//     });
+    it("Should not add a raffle to the raffles array because the contract doesn't have allowance for the prize amount", async function () {
+        await expect(raffleWorld.setRaffle("Test raffle", "1663346190", testToken.address, "1000000000000", "10", "1000000000", "2"))
+            .to.be.revertedWith("RaffleWorld: Insufficient tokens for prize poll");
+    });
 
-//     it("Shoud add a raffle to the raffles array", async function () {
-//         await testToken.approve(raffleWorld.address, "1000000000000");
-//         await expect(raffleWorld.setRaffle("Test raffle", "1663346190", testToken.address, "1000000000000", "10", "1000000000", "2")).to.not.be.reverted;
-//     });
-// })
+    it("Shoud add a raffle to the raffles array", async function () {
+        await testToken.approve(raffleWorld.address, "1000000000000");
+        await expect(raffleWorld.setRaffle("Test raffle", "1663346190", testToken.address, "1000000000000", "10", "1000000000", "2")).to.not.be.reverted;
+    });
+})
