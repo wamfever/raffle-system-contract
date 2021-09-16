817909e172e49cdb8c1844fef02a4da1ef4c7974
Alexandru <gazdac.alex21@gmail.com>
2021-09-16 15:18:06 +0300

    Hardhat sample project cleanup & hardhat.config setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,69 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
-
   for (const account of accounts) {
     console.log(account.address);
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.0',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: '0.6.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: '0.6.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    ]
+  },
+
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_RINKEBY_TESNET_URL,
+        accounts: {
+          mnemonic:"test test test test test test test test test test test junk",
+          initialIndex:0,
+          path:"m/44'/60'/0'/0",
+          count:20,
+          accountsBalance:"10000000000000000000000"
+        },
+        timeout: 90000
+      }
+    },
+    kovan: {
+      url: process.env.ALCHEMY_RINKEBY_TESNET_URL,
+      accounts: [ process.env.METAMASK_ACCOUNT_PRIVATE_KEY ]
+    } 
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  mocha: {
+    timeout: 90000
+  }
 };
